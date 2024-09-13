c39056eace84b31c4c497e3f5b49f27cdd258452
Sev <captain.sev@outlook.com>
2024-09-13 09:37:52 +0200

    added slidecontrol to imports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { default as Alert } from './components/Alert/Alert.vue';
 export { default as Login } from './components/Login/Login.vue';
 export { default as LoadingSpinner } from './components/LoadingSpinner/LoadingSpinner.vue';
 export { default as Sidebar } from './components/Layout/Sidebar/Sidebar.vue';
+export { default as SlideControl } from './components/Controls/SlideControl/SlideControl.vue';
 
 // Composables
 export { default as useResizeObserver } from './composables/resize-observer';
