4b1cecbca5daa10d64f1064d63dd4d858cb13bf2
Severino <severin.opel@hotmail.com>
2025-06-03 10:16:11 +0200

    Added language text to code.

--- src/components/Display/Code/Code.stories.ts
+++ src/components/Display/Code/Code.stories.ts
@@ -10,29 +10,89 @@ export default meta;
 
 type Story = StoryObj<typeof Code>;
 
-export const Default: Story = {
-    args: {
-        value: `function exampleCode() {
-    try {
-        for(let i = 0; i < 10; i++) {
-            console.log("This is line " + (i + 1));
-        }
-
-        let isRunning = true;
-        let count = 0;
-        while(isRunning) {
-            console.log("Count is " + count);
-            count++;
-            if(count >= 5) {
-                isRunning = false;
+const exampleCode = `/**
+ * Sorting an array by 'Bubbling' the largest element to the end of the array
+ * 
+ * @param arr 
+ * @returns 
+ */
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-
-        return 'Example code executed successfully!';
-    }catch(e) {
-        return 'An error occurred:' + e;
     }
+    return arr;
+}
+
+/**
+ * Sorting an array by selecting the smallest element and placing it at the beginning
+ * 
+ * @param arr 
+ * @returns 
+ */
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        const temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
+
+/**
+ * Sorts an arry by utilizing the divide and conquer strategy
+ * 
+ * @param arr 
+ * @returns 
+ */
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return quickSort(left).concat(pivot, quickSort(right));
 }`
+
+export const Default: Story = {
+    args: {
+        value: exampleCode,
+    },
+    render: (args: any) => ({
+        components: { Code },
+        setup() {
+            return { args };
+        },
+        template: '<Code v-bind="args" />',
+    }),
+};
+
+export const WithLanguage: Story = {
+    args: {
+        value: exampleCode,
+        language: 'typescript',
     },
     render: (args: any) => ({
         components: { Code },
--- src/components/Display/Code/Code.vue
+++ src/components/Display/Code/Code.vue
@@ -3,10 +3,17 @@
         class="code position-relative overflow-auto"
         :data-theme="dark ? 'dark' : 'light'"
     >
+        <header
+            v-if="highlightedValue?.language"
+            style="font-size: 0.8rem;"
+            class="bg-white text-secondary position-absolute top-0 start-0 w-100 px-4 py-2 border border-bottom-0 rounded-top"
+        >
+            {{ capitalize(highlightedValue.language) }}
+        </header>
         <pre
-            class="hljs form-control h-100 py-4 px-4 m-0"
+            class="hljs form-control h-100 py-4 px-4 pt-5 m-0"
             v-if="highlightedValue"
-        ><code v-html="highlightedValue" /></pre>
+        ><code v-html="highlightedValue.value" /></pre>
     </div>
 </template>
 
@@ -15,17 +22,24 @@
     lang="ts"
 >
     import hljs from 'highlight.js/lib/common';
+    import { capitalize } from 'lodash';
     import { computed } from 'vue';
 
 
     const props = defineProps<{
         value: string;
-        dark?: boolean; // Optional prop to determine if dark mode is enabled
+        language?: string;
+        dark?: boolean;
+        showLanguage?: boolean;
     }>();
 
     const highlightedValue = computed(() => {
-        const formatted = hljs.highlightAuto(props.value);
-        return formatted.value;
+        // Only set languages if a specific language is provided
+        // otherwise, highlight.js will detect the language automatically.
+        const languages = props.language
+            ? [props.language]
+            : undefined;
+        return hljs.highlightAuto(props.value, languages);
     });
 </script>
 
