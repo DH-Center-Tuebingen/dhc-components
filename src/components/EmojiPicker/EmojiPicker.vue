6777650081a25bb664e0ef331e4222e1ed2f1b02
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2025-07-09 14:35:52 +0200

    allow two axis positioning

--- src/components/EmojiPicker/EmojiPicker.vue
+++ src/components/EmojiPicker/EmojiPicker.vue
@@ -97,11 +97,30 @@
     });
 
     const positionClasses = computed(() => {
-        const classes = ['top-0'];
-        if(props.position == 'start') {
-            classes.push('pe-1', 'end-100');
-        } else {
+        const classes: Array<string> = [];
+        let xDefault = true;
+        let yDefault = true;
+        const directions = props.position.split(' ');
+        directions.forEach(dir => {
+            if(dir == 'start') {
+                classes.push('pe-1', 'end-100');
+                xDefault = false;
+            } else if(dir == 'end') {
+                classes.push('ps-1', 'start-100');
+                xDefault = false;
+            } else if(dir == 'top') {
+                classes.push('bottom-0');
+                yDefault = false;
+            } else if(dir == 'bottom') {
+                classes.push('top-0');
+                yDefault = false;
+            }
+        });
+        if(xDefault) {
             classes.push('ps-1', 'start-100');
+        }
+        if(yDefault) {
+            classes.push('top-0');
         }
         return classes;
     });
