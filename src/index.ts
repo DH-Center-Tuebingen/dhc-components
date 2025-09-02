8f3ad92e20efc569e81166baf28be44efb04dad1
Vinzenz Rosenkranz <vinzenz.rosenkranz@uni-tuebingen.de>
2025-09-02 15:09:50 +0200

    add missing exports

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,23 @@ export { default as Toggle } from './components/Button/Toggle/Toggle.vue';
 export { default as IconButton } from './components/Button/IconButton/IconButton.vue';
 
 // Form
+export { default as Attribute } from './components/Form/Attribute/Attribute.vue';
+export { default as Boolean } from './components/Form/Boolean/Boolean.vue';
 export { default as Datepicker } from './components/Form/Datepicker/Datepicker.vue';
+export { default as Dimension } from './components/Form/Dimension/Dimension.vue';
 export { default as Double } from './components/Form/Double/Double.vue';
+export { default as Iconclass } from './components/Form/Iconclass/Iconclass.vue';
 export { default as Integer } from './components/Form/Integer/Integer.vue';
+export { default as List } from './components/Form/List/List.vue';
 export { default as Multiselect } from './components/Form/Multiselect/Multiselect.vue';
+export { default as Range } from './components/Form/Range/Range.vue';
+export { default as Rism } from './components/Form/Rism/Rism.vue';
+export { default as SiUnit } from './components/Form/SiUnit/SiUnit.vue';
+export { default as String } from './components/Form/String/String.vue';
+export { default as Stringfield } from './components/Form/Stringfield/Stringfield.vue';
 export { default as TFA } from './components/Form/TFA/TFA.vue';
+export { default as Timeperiod } from './components/Form/Timeperiod/Timeperiod.vue';
+export { default as Url } from './components/Form/Url/Url.vue';
 
 // Indicator
 export { default as DotIndicator } from './components/Indicators/Dot/Dot.vue';
@@ -22,9 +34,10 @@ export { default as DotIndicator } from './components/Indicators/Dot/Dot.vue';
 // Layout
 export { default as Accordion } from './components/Layout/Accordion/Accordion.vue';
 export { default as Container } from './components/Layout/Container/Container.vue';
-export { default as List } from './components/Layout/List/List.vue';
+export { default as LayoutList } from './components/Layout/List/List.vue';
 export { default as Menu } from './components/Layout/Menu/Menu.vue';
 export { default as Sidebar } from './components/Layout/Sidebar/Sidebar.vue';
+export { default as Spacer } from './components/Layout/Spacer/Spacer.vue';
 export { default as Tabs } from './components/Layout/Tabs/Tabs.vue';
 export { default as Tree } from './components/Layout/Tree/Tree.vue';
 export { default as Toolbar } from './components/Layout/Toolbar/Toolbar.vue';
