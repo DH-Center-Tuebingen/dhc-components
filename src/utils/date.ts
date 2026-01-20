7914a56863667e1fa87991746e8e00d4256fff6c
Vinzenz Rosenkranz <vinzenz.rosenkranz@posteo.de>
2026-01-21 00:47:16 +0100

    move date related helper functions from other projects

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,11 @@
-import dayjs from 'dayjs'
-import customParseFormat from 'dayjs/plugin/customParseFormat'
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(customParseFormat);
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
 export default dayjs;
 
@@ -15,10 +19,33 @@ const validateString = (date: string|null) => {
     return dayjs(date, acceptedFormats, true).isValid();
 }
 
+const toDayjs = (value: string | number) => {
+    if(value) {
+        if(value as number) {
+            value = (value as number) * 1000;
+        }
+        return dayjs.utc(value);
+    }
+}
+
 export const validate = (date: string | Array<string> | null) => {
     if(Array.isArray(date)) {
         return date.every(d => validateString(d));
     } else {
         return validateString(date);
+    }
+}
+
+export const date2string = (value: string | number) => {
+    const parsedValue = toDayjs(value);
+    if(parsedValue) {
+        return parsedValue.toDate().toString();
+    }
+}
+
+export const ago = (value: string | number) => {
+    const parsedValue = toDayjs(value);
+    if(parsedValue) {
+        return parsedValue.fromNow();
     }
 }
