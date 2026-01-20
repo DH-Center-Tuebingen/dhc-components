a9585d7ce0372d32014bef33583ef1fbc4b39eee
Vinzenz Rosenkranz <vinzenz.rosenkranz@posteo.de>
2026-01-21 00:46:05 +0100

    allow to extend local i18n with global messages

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,4 +1,6 @@
 import { createI18n } from 'vue-i18n';
+import * as de from './de.json';
+import * as en from './en.json';
 
 declare global {
     interface Window {
@@ -6,7 +8,21 @@ declare global {
     }
 }
 
-export const initI18n = (messages: object) => {
+export const initI18n = (messages: object, extendGlobal: boolean = false) => {
+    if(extendGlobal) {
+        if(messages.de) {
+            messages.de = {
+                ...de,
+                ...messages.de,
+            };
+        }
+        if(messages.en) {
+            messages.en = {
+                ...en,
+                ...messages.en,
+            };
+        }
+    }
     return createI18n<false, typeof messages>({
         locale: window?.globalLocale || 'en',
         fallbackLocale: 'en',
